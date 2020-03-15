f659c74ee16f00ab56f09a7e930dd5be2907e378
Sam <Sam@DESKTOP-4FRQ768>
2020-03-15 14:40:16 +0100

    roundrobin toegevoegd

--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -9,8 +9,38 @@ public class RoundRobin extends Scheduler{
 
 	@Override
 	public PriorityQueue<Process> schedule(PriorityQueue<Process> processen) {
-		// TODO Auto-generated method stub
-		return null;
+		PriorityQueue<Process> scheduled = new PriorityQueue<Process>();
+		
+		int huidigeTijd=0;
+		while(!processen.isEmpty()) {
+			Process tijdelijk= processen.poll();
+			if(tijdelijk.getArrivalTime()>huidigeTijd)huidigeTijd=tijdelijk.getArrivalTime();			
+			
+			tijdelijk.setStartTijd(huidigeTijd);
+			int serviceTime=tijdelijk.getServiceTime();
+			if(timeSlice<serviceTime) {
+				serviceTime-=timeSlice;
+				huidigeTijd+=timeSlice;
+				processen.add(tijdelijk);
+			}
+			else {
+				huidigeTijd+=serviceTime;
+				tijdelijk.setEndTijd(huidigeTijd);
+				tijdelijk.rekenUit();
+				scheduled.add(tijdelijk);
+			}
+		}
+		
+		//berekening statistieken
+				for(Process p:scheduled) {
+					gemWachttijd+=p.getWachtTijd();
+					gemOmlooptijd+=p.getOmloopTijd();
+					gemNormOmlooptijd+=p.getNormOmloopTijd();
+				}
+				gemWachttijd/=scheduled.size();
+				gemOmlooptijd/=scheduled.size();
+				gemNormOmlooptijd/=scheduled.size();		
+				return scheduled;
 	}
 
 }
