264c65f0006e0d75e6e97c4810a6dda1601cc999
wille <willem.juwet@gmail.com>
2020-03-20 10:32:33 +0100

    toevoegen van outputbestanden

--- src/Main.java
+++ src/Main.java
@@ -2,121 +2,143 @@ import java.util.PriorityQueue;
 
 public class Main {
 	public static void main(String[] args){
-		int aantalProcessen=10000; //10000-20000-50000
-		
-		XMLParser parser=new XMLParser();
-		parser.leesProcessen(aantalProcessen);
-		
-		System.out.println("Aantal Processen: " + aantalProcessen);
-		PriorityQueue<Process> processen=parser.getProcessen();
-		
-		Scheduler firstComeFirstServed = new FirstComeFirstServed();
-		PriorityQueue<Process> hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
+		int[] aantallen={10000,20000,50000};
+		for(int i=0;i<3;i++) {
+			int aantalProcessen=aantallen[i];
+			XMLParser parser=new XMLParser();
+			parser.leesProcessen(aantalProcessen);
+
+			System.out.println("Aantal Processen: " + aantalProcessen);
+			PriorityQueue<Process> processen=parser.getProcessen();
+
+			PriorityQueue<Process> hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			PriorityQueue<Process> outputProcessen = new PriorityQueue<>();
+
+			Scheduler firstComeFirstServed = new FirstComeFirstServed();
+			outputProcessen=firstComeFirstServed.schedule(hulpProcessen);	
+			System.out.println("First Come First Served:");
+			System.out.println("	Gemiddelde wachtijd: " + firstComeFirstServed.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + firstComeFirstServed.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + firstComeFirstServed.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//FCFS.xml");
+			hulpProcessen.clear();
+
+			Scheduler shortestJobFirst = new ShortestJobFirst();
+			hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			outputProcessen=shortestJobFirst.schedule(hulpProcessen);	
+			System.out.println("Shortest Job First:");
+			System.out.println("	Gemiddelde wachtijd: " + shortestJobFirst.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + shortestJobFirst.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + shortestJobFirst.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//SJF.xml");
+			hulpProcessen.clear();
+
+			Scheduler shortestRemainingTime = new ShortestRemainingTime();
+			hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			outputProcessen=shortestRemainingTime.schedule(hulpProcessen);	
+			System.out.println("Shortest Remaining Time:");
+			System.out.println("	Gemiddelde wachtijd: " + shortestRemainingTime.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + shortestRemainingTime.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + shortestRemainingTime.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//SRT.xml");
+			hulpProcessen.clear();
+
+			Scheduler roundRobin2 = new RoundRobin(2);
+			hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			outputProcessen=roundRobin2.schedule(hulpProcessen);	
+			System.out.println("Round Robin(timeslice q=2):");
+			System.out.println("	Gemiddelde wachtijd: " + roundRobin2.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + roundRobin2.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + roundRobin2.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//RR2.xml");
+			hulpProcessen.clear();
+
+			Scheduler roundRobin4 = new RoundRobin(4);
+			hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			outputProcessen=roundRobin4.schedule(hulpProcessen);
+			System.out.println("Round Robin(timeslice q=4):");
+			System.out.println("	Gemiddelde wachtijd: " + roundRobin4.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + roundRobin4.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + roundRobin4.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//RR4.xml");
+			hulpProcessen.clear();
+
+			Scheduler roundRobin8 = new RoundRobin(8);
+			hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			outputProcessen=roundRobin8.schedule(hulpProcessen);
+			System.out.println("Round Robin(timeslice q=8):");
+			System.out.println("	Gemiddelde wachtijd: " + roundRobin8.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + roundRobin8.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + roundRobin8.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//RR8.xml");
+			hulpProcessen.clear();	
+
+			Scheduler highestResponseRatioNext = new HighestResponseRatioNext();
+			hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			outputProcessen=highestResponseRatioNext.schedule(hulpProcessen);	
+			System.out.println("Highest Response Ratio Next:");
+			System.out.println("	Gemiddelde wachtijd: " + highestResponseRatioNext.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + highestResponseRatioNext.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + highestResponseRatioNext.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//HRRN.xml");
+			hulpProcessen.clear();
+
+
+			Scheduler multilevelFeedback = new MultilevelFeedback();
+			hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			outputProcessen=multilevelFeedback.schedule(hulpProcessen);	
+			System.out.println("Multilevel Feedback:");
+			System.out.println("	Gemiddelde wachtijd: " + multilevelFeedback.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + multilevelFeedback.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + multilevelFeedback.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//MLF.xml");
+			hulpProcessen.clear();
+
+			Scheduler multilevelFeedbackV2 = new MultilevelFeedbackV2();
+			hulpProcessen = new PriorityQueue<>();
+			for(Process p:processen) {
+				hulpProcessen.add(new Process(p));
+			}
+			outputProcessen=multilevelFeedbackV2.schedule(hulpProcessen);	
+			System.out.println("Multilevel Feedback versie 2:");
+			System.out.println("	Gemiddelde wachtijd: " + multilevelFeedbackV2.gemWachttijd);
+			System.out.println("	Gemiddelde omlooptijd: " + multilevelFeedbackV2.gemOmlooptijd);
+			System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + multilevelFeedbackV2.gemNormOmlooptijd);
+			parser.setOutputProcessen(outputProcessen);
+			parser.schrijfProcessen(aantalProcessen+"//MLF2.xml");
+			hulpProcessen.clear();
 		}
-		firstComeFirstServed.schedule(hulpProcessen);	
-		System.out.println("First Come First Served:");
-		System.out.println("	Gemiddelde wachtijd: " + firstComeFirstServed.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + firstComeFirstServed.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + firstComeFirstServed.gemNormOmlooptijd);
-		hulpProcessen.clear();
-		
-		Scheduler shortestJobFirst = new ShortestJobFirst();
-		hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
-		}
-		shortestJobFirst.schedule(hulpProcessen);	
-		System.out.println("Shortest Job First:");
-		System.out.println("	Gemiddelde wachtijd: " + shortestJobFirst.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + shortestJobFirst.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + shortestJobFirst.gemNormOmlooptijd);
-		hulpProcessen.clear();
-		
-		Scheduler shortestRemainingTime = new ShortestRemainingTime();
-		hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
-		}
-		shortestRemainingTime.schedule(hulpProcessen);	
-		System.out.println("Shortest Remaining Time:");
-		System.out.println("	Gemiddelde wachtijd: " + shortestRemainingTime.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + shortestRemainingTime.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + shortestRemainingTime.gemNormOmlooptijd);
-		hulpProcessen.clear();
-		
-		Scheduler roundRobin2 = new RoundRobin(2);
-		hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
-		}
-		roundRobin2.schedule(hulpProcessen);	
-		System.out.println("Round Robin(timeslice q=2):");
-		System.out.println("	Gemiddelde wachtijd: " + roundRobin2.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + roundRobin2.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + roundRobin2.gemNormOmlooptijd);
-		hulpProcessen.clear();
-		
-		Scheduler roundRobin4 = new RoundRobin(4);
-		hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
-		}
-		roundRobin4.schedule(hulpProcessen);
-		System.out.println("Round Robin(timeslice q=4):");
-		System.out.println("	Gemiddelde wachtijd: " + roundRobin4.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + roundRobin4.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + roundRobin4.gemNormOmlooptijd);
-		hulpProcessen.clear();
-		
-		Scheduler roundRobin8 = new RoundRobin(8);
-		hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
-		}
-		roundRobin8.schedule(hulpProcessen);
-		System.out.println("Round Robin(timeslice q=8):");
-		System.out.println("	Gemiddelde wachtijd: " + roundRobin8.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + roundRobin8.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + roundRobin8.gemNormOmlooptijd);
-		hulpProcessen.clear();	
-		
-		Scheduler highestResponseRatioNext = new HighestResponseRatioNext();
-		hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
-		}
-		highestResponseRatioNext.schedule(hulpProcessen);	
-		System.out.println("Highest Response Ratio Next:");
-		System.out.println("	Gemiddelde wachtijd: " + highestResponseRatioNext.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + highestResponseRatioNext.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + highestResponseRatioNext.gemNormOmlooptijd);
-		hulpProcessen.clear();
-		
-		
-		Scheduler multilevelFeedback = new MultilevelFeedback();
-		hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
-		}
-		multilevelFeedback.schedule(hulpProcessen);	
-		System.out.println("Multilevel Feedback:");
-		System.out.println("	Gemiddelde wachtijd: " + multilevelFeedback.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + multilevelFeedback.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + multilevelFeedback.gemNormOmlooptijd);
-		hulpProcessen.clear();
-		
-		Scheduler multilevelFeedbackV2 = new MultilevelFeedbackV2();
-		hulpProcessen = new PriorityQueue<>();
-		for(Process p:processen) {
-			hulpProcessen.add(new Process(p));
-		}
-		multilevelFeedbackV2.schedule(hulpProcessen);	
-		System.out.println("Multilevel Feedback versie 2:");
-		System.out.println("	Gemiddelde wachtijd: " + multilevelFeedbackV2.gemWachttijd);
-		System.out.println("	Gemiddelde omlooptijd: " + multilevelFeedbackV2.gemOmlooptijd);
-		System.out.println("	Gemiddelde genormaliseerde omlooptijd: " + multilevelFeedbackV2.gemNormOmlooptijd);
-		hulpProcessen.clear();
 	}
 }
--- src/XMLParser.java
+++ src/XMLParser.java
@@ -1,14 +1,26 @@
-import javax.xml.parsers.DocumentBuilderFactory;  
-import javax.xml.parsers.DocumentBuilder;  
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.parsers.DocumentBuilder;
+
 import org.w3c.dom.Document;  
 import org.w3c.dom.NodeList;  
 import org.w3c.dom.Node;  
 import org.w3c.dom.Element;  
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.PriorityQueue;  
 
 public class XMLParser {
 	public PriorityQueue<Process> processen;
+	public PriorityQueue<Process> outputProcessen;
 
 	public PriorityQueue<Process> getProcessen() {
 		return processen;
@@ -16,6 +28,13 @@ public class XMLParser {
 
 	public void setProcessen(PriorityQueue<Process> processen) {
 		this.processen = processen;
+	}
+	public PriorityQueue<Process> getOutputProcessen() {
+		return outputProcessen;
+	}
+
+	public void setOutputProcessen(PriorityQueue<Process> outputProcessen) {
+		this.outputProcessen = outputProcessen;
 	}
 
 	public XMLParser() {
@@ -48,4 +67,69 @@ public class XMLParser {
 			e.printStackTrace();  
 		}   
 	}
+	public void schrijfProcessen(String bestandsnaam) {
+
+		String xmlFilePath = bestandsnaam;
+		try {
+
+			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+
+			DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+
+			Document document = documentBuilder.newDocument();
+
+			// root element
+			Element root = document.createElement("processlist");
+			document.appendChild(root);
+
+			for(Process p: outputProcessen) {
+				// process element
+				Element process = document.createElement("process");
+				root.appendChild(process);
+				// id element
+				Element pid = document.createElement("pid");
+				pid.appendChild(document.createTextNode(String.valueOf(p.getId())));
+				process.appendChild(pid);
+				// bedieningstijd element
+				Element serviceTime = document.createElement("serviceTime");
+				serviceTime.appendChild(document.createTextNode(String.valueOf(p.getServiceTime())));
+				process.appendChild(serviceTime);
+				// wachttijd element
+				Element waitTime = document.createElement("waitTime");
+				waitTime.appendChild(document.createTextNode(String.valueOf(p.getWachtTijd())));
+				process.appendChild(waitTime);
+				// genormaliseerde omlooptijd element
+				Element normTAT = document.createElement("normTAT");
+				normTAT.appendChild(document.createTextNode(String.valueOf(p.getNormOmloopTijd())));
+				process.appendChild(normTAT);
+			}
+			// create the xml file
+			//transform the DOM Object to an XML File
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource domSource = new DOMSource(document);
+			
+			Path fileToDeletePath = Paths.get(xmlFilePath);
+			Files.deleteIfExists(fileToDeletePath);
+			File output=new File(xmlFilePath);
+			
+			StreamResult streamResult = new StreamResult(output);
+
+			// If you use
+			// StreamResult result = new StreamResult(System.out);
+			// the output will be pushed to the standard output ...
+			// You can use that for debugging 
+
+			transformer.transform(domSource, streamResult);
+
+			System.out.println("Done creating XML File"+xmlFilePath);
+
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (TransformerException tfe) {
+			tfe.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	} 
 }  
